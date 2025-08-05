f5cf2502e3bc568f22ecfc8ce97a8b058872f939
Prithiraj57 <prithirajbiswas69@gmail.com>
2025-08-06 01:20:19 +0530

    updated the url

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,2 +1,2 @@
-export const URL="http://localhost:5000"
-export const IF="http://localhost:5000/images/"
+export const URL="https://blog-app-suxu.onrender.com"
+export const IF="https://blog-app-suxu.onrender.com/images/"
